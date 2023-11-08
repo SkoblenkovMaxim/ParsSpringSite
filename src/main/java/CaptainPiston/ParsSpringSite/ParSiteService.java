37622c754b27a5076668d918951375287dd58e74
Maks <goofy.net@yandex.ru>
2023-11-09 01:22:36 +0400

    Максим: добавил зависимость Spring web, контроллер Максим_22.10.2023_добавил список ArrayList, объект ParSiteData и вывод в консоль из списка Максим_27.10.2023: исправил архитектуру (было не правильно составлено) => MVC Максим_29.10.2023: в ParsSiteApplication добавил цикл в котором создаются объекты ParSiteData и записываются в List<> Максим_30.10.2023: List<> и вывел из цикла Максим_02.11.2023: добавил class ParSiteService и уточнил класс ParSiteController Максим_09.11.2023: добавил файл application.yml, уточнил класс ParSiteService

--- src/main/java/CaptainPiston/ParsSpringSite/ParSiteService.java
+++ src/main/java/CaptainPiston/ParsSpringSite/ParSiteService.java
@@ -1,9 +1,13 @@
 package CaptainPiston.ParsSpringSite;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParSiteService {
+    @ConfigurationProperties(prefix = "url")
+    @Bean
     public String home() {
         return "polls";
     }
--- src/main/java/CaptainPiston/ParsSpringSite/controllers/ParSiteController.java
+++ src/main/java/CaptainPiston/ParsSpringSite/controllers/ParSiteController.java
@@ -1,16 +1,22 @@
 package CaptainPiston.ParsSpringSite.controllers;
 
 import CaptainPiston.ParsSpringSite.ParSiteService;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 
+@Component
 @RestController
 public class ParSiteController {
-    @GetMapping("/")
-    public String parSiteService() {
-        return new ParSiteService().home();
+    private final ParSiteService parSiteService;
+
+    @Autowired
+    public ParSiteController(ParSiteService parSiteService) {
+        this.parSiteService = parSiteService;
     }
-//    public String home() {
-//        return "polls";
-//    }
+
+    @Override
+    public String toString() {
+        return " " + parSiteService;
+    }
 }
