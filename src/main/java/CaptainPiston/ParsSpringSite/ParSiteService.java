b2384e5061d314b9fac3eb7224e0d9eeb2d71d0e
Maks <goofy.net@yandex.ru>
2023-11-10 01:01:48 +0400

    Максим: добавил зависимость Spring web, контроллер Максим_22.10.2023_добавил список ArrayList, объект ParSiteData и вывод в консоль из списка Максим_27.10.2023: исправил архитектуру (было не правильно составлено) => MVC Максим_29.10.2023: в ParsSiteApplication добавил цикл в котором создаются объекты ParSiteData и записываются в List<> Максим_30.10.2023: List<> и вывел из цикла Максим_02.11.2023: добавил class ParSiteService и уточнил класс ParSiteController Максим_09.11.2023: добавил файл application.yml, уточнил класс ParSiteService Максим_10.11.2023: доработал контроллер (добавил рест метод, который выводит строку в браузере) и сервис (добавил связь с yaml)

--- src/main/java/CaptainPiston/ParsSpringSite/ParSiteService.java
+++ src/main/java/CaptainPiston/ParsSpringSite/ParSiteService.java
@@ -1,14 +1,13 @@
 package CaptainPiston.ParsSpringSite;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParSiteService {
-    @ConfigurationProperties(prefix = "url")
-    @Bean
-    public String home() {
-        return "polls";
+    @Value("${url}")
+    String url;
+    public String serviceHome() {
+        return url;
     }
 }
--- src/main/java/CaptainPiston/ParsSpringSite/controllers/ParSiteController.java
+++ src/main/java/CaptainPiston/ParsSpringSite/controllers/ParSiteController.java
@@ -1,8 +1,8 @@
 package CaptainPiston.ParsSpringSite.controllers;
 
 import CaptainPiston.ParsSpringSite.ParSiteService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Component
@@ -10,13 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ParSiteController {
     private final ParSiteService parSiteService;
 
-    @Autowired
     public ParSiteController(ParSiteService parSiteService) {
         this.parSiteService = parSiteService;
     }
-
     @Override
     public String toString() {
         return " " + parSiteService;
+    }
+    @GetMapping("/")
+    public String home() {
+        return parSiteService.serviceHome();
     }
 }
